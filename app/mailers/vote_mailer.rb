1e0df6c2d0a22e69783e8ff300145c0c69d87aa1
Nora Delaney <nora.delaney@gmail.com>
2015-07-23 11:47:54 -0400

    mailer

--- app/mailers/vote_mailer.rb
+++
@@ -1,7 +0,0 @@
-class VoteMailer < ApplicationMailer
-  def new_vote(vote)
-    @vote = vote
-    subject = "You are someone's favorite!"
-    mail(to: vote.user.email, subject: subject)
-  end
-end
