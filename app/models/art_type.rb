6dda3c1c25d42eb420d9b1ecea49c7e03f10fb40
Nora Delaney <nora.delaney@gmail.com>
2015-08-06 08:54:05 -0400

    wip

--- app/models/art_type.rb
+++ app/models/art_type.rb
@@ -3,6 +3,15 @@ class ArtType < ActiveRecord::Base
   has_many :users, through: :user_arts
   accepts_nested_attributes_for :user_arts
 
+  VALID_TYPES = [
+    "visual art",
+    "writing",
+    "music",
+    "dance",
+    "theater",
+    "filmmaking"
+  ]
+
   validates :name, presence: true, inclusion: { in: VALID_TYPES }
 
 end
