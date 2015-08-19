a4cf0764581a5af25e8547d4e87ea6e7334b4a5f
Nora Delaney <nora.delaney@gmail.com>
2015-08-19 19:07:44 -0400

    admin rspec

--- spec/features/admin_views_users_spec.rb
+++ spec/features/admin_views_users_spec.rb
@@ -1,15 +1,23 @@
-# require 'rails_helper'
-#
+require 'rails_helper'
 
-# feature "admin views users" do
-# # As an administrator of Make With Me
-# # I want to see a list of users and email addresses
-# # So that I can view who is using my website.
-# #
-# #
-# # Acceptance Criteria
-# # [ ] I can see a link to view a list of users from the homepage
-# # [ ] The page should contain the user's full name and email address.
-# # [ ] If I am not an administrator, I should be redirected to the homepage.
+
+feature "admin views users" do
+# As an administrator of Make With Me
+# I want to see a list of users and email addresses
+# So that I can view who is using my website.
 #
-# end
+#
+# Acceptance Criteria
+# [ ] I can see a link to view a list of users from the homepage
+# [ ] The page should contain the user's full name and email address.
+# [ ] If I am not an administrator, I should be redirected to the homepage.
+
+let(:admin) { FactoryGirl.create(:user, role: 'admin') }
+let!(:user) { FactoryGirl.create(:user) }
+
+  scenario 'admin visits users index' do
+    login_as(admin)
+    visit '/admin/users'
+    expect(page).to have_content(user.email)
+  end
+end
