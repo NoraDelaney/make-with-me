c6b64ed70f2d2f69b2b00e350165b88ffd346a88
Nora Delaney <nora.delaney@gmail.com>
2015-08-20 23:19:02 -0400

    hound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def authorize_admin!
-    if current_user.nil? or !current_user.is_admin?
+    if current_user.nil? || !current_user.is_admin?
       flash[:notice] = "You are not authorized to view this resource."
       redirect_to_root_path
     end
